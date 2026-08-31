2078904c015739b1aeb024c01c8edc952fb1c3fc
tejagandham03 <tejagandham03@gmail.com>
2026-08-31 12:01:46 +0530

    msg

---
+++ 06-folder/myapp/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-500'>
+      <h1 className='pen'>this is a pen</h1>
+    </div>
+  )
+}
+
+export default App
